3e9a30ac3cd0a53640224fd0024df2159eae7af1
nick bilotto <nickbilotto@gmail.com>
2024-10-23 14:42:59 -0700

    Button Added

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -1,6 +1,7 @@
 package edu.miracosta.cs112.lab07;//package name here depending on your IDE
 
 import javafx.application.Application;  //abstract class used for JavaFX GUI's
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;              //class for GUI window
 import javafx.scene.Scene;              //class for specific view in GUI window
@@ -10,7 +11,8 @@ import javafx.scene.control.Button;     //class for button component
 import javafx.event.EventHandler;       //interface for handling events
 import javafx.event.ActionEvent;        //class for type of event for action (like button or key pressed)
 
-public class HelloApplication extends Application  { //inheriting core functionality + this class will handle events
+
+public class HelloApplication extends Application implements EventHandler<ActionEvent> { //inheriting core functionality + this class will handle events
     /*** GUI COMPONENTS ***/
     // TODO: follow step 25 in README.md to create reference variables
 
@@ -21,6 +23,8 @@ public class HelloApplication extends Application  { //inheriting core functiona
 
     // TODO: follow steps 2-9 in README.md to create a start method
 
+    Button button;
+    Label label;
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("hello GUI: Nick Bilotto");
@@ -30,10 +34,29 @@ public class HelloApplication extends Application  { //inheriting core functiona
         Scene scene = new Scene(layout, 400, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
-        //asdfas
+
+        label = new Label("Hello World!");
+        layout.getChildren().add(label);
+
+        AnchorPane anchorPane = new AnchorPane();
+        anchorPane.setBottomAnchor(label, 0.0);
+        anchorPane.setRightAnchor(label, 0.0);
+        anchorPane.getChildren().add(label);
+        layout.getChildren().add(anchorPane);
+
+        button = new Button("Click Me");
+        anchorPane.setBottomAnchor(button, 0.0);
+        anchorPane.setLeftAnchor(button, 0.0);
+        anchorPane.getChildren().add(button);
+        button.setOnAction(this);
+
 
     }
-
+    public void handle(ActionEvent e){
+        if (e.getSource() == button){
+            label.setText("Goobye World");
+        }
+    }
 
     // TODO: follow steps 10-21 in README.md to add objects to your layout (inside start)
 
